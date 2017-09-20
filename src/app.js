cf4010bf55373ee93c136c342602d13045e685f6
Yann Carlen <yann-carlen@hotmail.fr>
2017-09-20 17:32:11 +0200

    arch

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser')
 
 require('dotenv').config()
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+const index = require('./routes/index/index')
+const repo = require('./routes/repo/repo')
 
 const app = express()
 const server = require('http').createServer(app)
@@ -29,7 +29,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
-app.use('/users', users)
+app.use('/repo', repo)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -43,7 +43,6 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
   // render the error page
   res.status(err.status || 500)
   res.render('error')
