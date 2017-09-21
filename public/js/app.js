5e186114f7d54a162dd5c10590223fe24005f28c
Yann Carlen <yann-carlen@hotmail.fr>
2017-09-21 15:48:57 +0200

    add mithril storage

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,27 @@
 const root = document.getElementById('app')
 
-m.render(root, m('button', 'Se connecter'))
+const wesh = console.log
+
+mx.storage(mx.DEFAULT_STORAGE_NAME, mx.SESSION_STORAGE)
+
+const storage = mx.storage()
+
+const state = {
+  username: '',
+  login: false
+}
+
+const openModal = () => {}
+
+const Login = {
+  view: () => m('main', [
+    m('button', { onclick: openModal, class: 'button' }, 'Login')
+  ])
+}
+
+const Repo = {
+} 
+
+const { login } = storage.get('state') || { login: false }
+
+m.mount(root, !login ? Login : Repo)
